557e5d0f9f3cbd4e783e1cc3328778407ad4459f
Daniel Zapata <danielzapata1019@gmail.com>
2018-01-15 22:41:28 -0500

    Init Webhook

--- controller/messageWebhook.js
+++ controller/messageWebhook.js
@@ -1,1 +1,30 @@
 'use strict'
+
+const request = require('request')
+const processMessage = require('../helpers/processMessage');
+
+module.exports=(req,res)=>{
+    var data=req.body;  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      data.entry.forEach(function(entry) {
+        
+        var pageId=entry.id;
+        var timeOfEvent = entry.time;
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+
+  };
+
--- index.js
+++ index.js
@@ -4,11 +4,58 @@
 const
   express = require('express'),
   bodyParser = require('body-parser');
- 
-const app=express();
-const PORT= process.env.PORT || 3000;
+
+/*const verificationController = require('./controllers/verification');
+const messageWebhookController = require('./controllers/messageWebhook');*/
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 // Sets server port and logs message on success
 
-const server = app.listen(PORT, () => {
-    console.log('Express server listening on port %d', server.address().port);
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended : true }));
+
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === 'say_hello') {
+    res.send(req.query['hub.challenge'])
+  } else {
+    res.send('Access denied!!');
+  }
 });
+
+app.post('/webhook', (req, res) => {
+  var data = req.body;
+  if (data.object == 'page') {
+    data.entry.forEach(pageEntry => {
+      pageEntry.messaging.forEach(messagingEvent => {
+        if (messagingEvent.message) {
+          console.log(messagingEvent);
+          receiveMessage(messagingEvent);
+        }
+
+      })
+    });
+    res.sendStatus(200);
+  }
+});
+
+function receiveMessage(event) {
+  var senderId = event.sender.id;
+  var messageText = event.message.text;
+  console.log(event);
+  console.log(senderId);
+  console.log(messageText);
+};
+
+//app.get('/webhook', verificationController); //ruta
+//app.post('/webhook', messageWebhookController);
+
+const server = app.listen(PORT, () => {
+  console.log('Express server listening on port %d', server.address().port);
+});
+
+
+const APP_TOKEN = 'EAABZC9Ls6FtYBAKiA1jjSj2mdSreFoEGjHX7wSNEXJpyOChdDyqbp8QJBtQTeFwyIyqgpnTOydAhfsJrg6aCBycmf1QjWvSagM0ZA75D5ihmf0XpvZB81odQyBNGppPMUTY3ZAPezTAZBCFab9Ia0v7HyIVwYhSdnqCNSdSQ7ewZDZD';
+
+
+
